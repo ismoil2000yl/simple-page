07d95fe4690c5a97b7d82b222d072778d3bf7a90
Ismoiljon Jalolov <94523115+ismoil2000yl@users.noreply.github.com>
2023-06-23 17:33:09 +0300

    last-push

--- src/components/layout/header/index.jsx
+++ src/components/layout/header/index.jsx
@@ -1,6 +1,8 @@
 import IconLogo from 'assets/images/png/Logo.png'
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
+import IconLeft from 'assets/images/png/left.png'
+import IconShare from 'assets/images/png/share.png'
 
 const header = () => {
 
@@ -8,38 +10,56 @@ const header = () => {
   const [langActive, setLangActive] = useState(1)
 
   const navigate = useNavigate()
+  const location = useLocation()
 
   return (
     <header className='header'>
       <div className="container">
-        <div className="header__wrapper">
+        <div className={`header__wrapper ${location.pathname.toLowerCase().includes('page') ? 'header-none' : ''}`}>
           <div className="header__wrapper__nav">
-            <div className="header__wrapper__nav__logo" onClick={()=>navigate('/')}>
+            <div className="header__wrapper__nav__logo" onClick={() => navigate('/')}>
               <img src={IconLogo} alt="" />
             </div>
             <nav className="navbar">
               <ul className='navbar-box'>
-                <li className={`navbar-box-item ${menuActive == 1 ? `active` : ''}`} onClick={()=>setMenuActive(1)}>
-                  <a  className='navbar-box-item-link'>Texno Yangiliklar</a>
+                <li className={`navbar-box-item ${menuActive == 1 ? `active` : ''}`} onClick={() => setMenuActive(1)}>
+                  <a className='navbar-box-item-link'>Texno Yangiliklar</a>
                 </li>
-                <li className={`navbar-box-item ${menuActive == 2 ? `active` : ''}`} onClick={()=>setMenuActive(2)}>
+                <li className={`navbar-box-item ${menuActive == 2 ? `active` : ''}`} onClick={() => setMenuActive(2)}>
                   <a className='navbar-box-item-link'>Avto Yangiliklar</a>
                 </li>
-                <li className={`navbar-box-item ${menuActive == 3 ? `active` : ''}`} onClick={()=>setMenuActive(3)}>
+                <li className={`navbar-box-item ${menuActive == 3 ? `active` : ''}`} onClick={() => setMenuActive(3)}>
                   <a className='navbar-box-item-link'>Sharhlar</a>
                 </li>
               </ul>
             </nav>
           </div>
           <ul className="header__wrapper__language lang">
-            <li className={`lang-item ${langActive == 1 ? `active-lang` : ''}`} onClick={()=>setLangActive(1)}>
+            <li className={`lang-item ${langActive == 1 ? `active-lang` : ''}`} onClick={() => setLangActive(1)}>
               <a className='lang-item-title'>Uz</a>
             </li>
-            <li className={`lang-item ${langActive == 2 ? `active-lang` : ''}`} onClick={()=>setLangActive(2)}>
+            <li className={`lang-item ${langActive == 2 ? `active-lang` : ''}`} onClick={() => setLangActive(2)}>
               <a className='lang-item-title'>Ру</a>
             </li>
           </ul>
         </div>
+
+        {
+          location.pathname.toLowerCase().includes('page') ?
+            <div className="header__page">
+              <button className="header__page__btn" onClick={() => navigate(-1)}>
+                <img src={IconLeft} alt="" />
+              </button>
+              <button className="header__page__btn header__page__logo" onClick={() => navigate('/')}>
+                <img src={IconLogo} alt="" />
+              </button>
+              <button className="header__page__btn">
+                <img src={IconShare} alt="" />
+              </button>
+            </div>
+            : ''
+        }
+
       </div>
     </header>
   )
